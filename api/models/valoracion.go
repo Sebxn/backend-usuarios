6070448a17bf2e9b1bca1db6b71dc7778a8801db
Sebxn <snaranjo@utem.cl>
2023-12-02 19:51:50 -0300

    se le agrego comentario

--- api/models/valoracion.go
+++ api/models/valoracion.go
@@ -1,9 +1,10 @@
 package models
 
 type Valoracion struct {
-	ID        int `json:"id"`
-	IDReserva int `json:"id_reserva"`
-	Estrellas int `json:"estrellas"`
+	ID         int    `json:"id"`
+	IDReserva  int    `json:"id_reserva"`
+	Estrellas  int    `json:"estrellas"`
+	Comentario string `json:"comentario"`
 }
 
 func (Valoracion) TableName() string {
